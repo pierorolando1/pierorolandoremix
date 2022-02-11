c9da1bcd0128919d5d992a486403a355ed154350
Piero Rolando <rolandopiero63@gmail.com>
2022-02-11 16:15:30 -0500

    dasdfhsakfasd

--- app/lib/mdx.ts
+++ app/lib/mdx.ts
@@ -4,10 +4,13 @@ import matter from 'gray-matter'
 import { serialize } from 'next-mdx-remote/serialize'
 //@ts-ignore
 import mdxPrism from 'mdx-prism'
+import { log } from '~/utils'
 
 const root = process.cwd()
 
 export const getFileBySlug = async (slug: string) => {
+
+  log(root)
 
   const file = path.join(root, "posts", `${slug}.mdx`)
   const mdxSource = fs.readFileSync(file, "utf8");
