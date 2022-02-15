c8ec3b162d5f337a0e7a7298075da69a36312ad8
Piero Rolando <rolandopiero63@gmail.com>
2022-02-15 10:02:46 -0500

    i fell like a god

--- app/routes/admin/edit/$id.tsx
+++ app/routes/admin/edit/$id.tsx
@@ -1,6 +1,7 @@
-import { Button } from "@nextui-org/react"
+import { Button, Modal } from "@nextui-org/react"
 import { DataFunctionArgs } from "@remix-run/server-runtime";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { useLoaderData } from "remix";
 import MarkdownMonaco from "~/components/markdown_with_monaco"
@@ -20,19 +21,30 @@ export default () => {
 
   const { content } = useSelector(state => (state as any).admin)
 
+  const [modal, setModal] = useState({ open: false, message: "", color: "text-green-500" })
+
   const loaderData = useLoaderData()
 
   const handleUpdate = async () => {
-    const postRef = doc(db, "posts_remix", loaderData.params.id as string)
+    try {
+      const postRef = doc(db, "posts_remix", loaderData.params.id as string)
+      await updateDoc(postRef, {
+        content
+      })
 
-    // Set the "capital" field of the city 'DC'
-    await updateDoc(postRef, {
-      content
-    })
+      setModal({ open: true, message: "Post updated", color: "text-green-500" })
+    } catch (error) {
+      setModal({ open: true, message: error + '', color: "text-red-500" })
+      console.error(error)
+    }
   }
 
   return (
     <>
+      <Modal open={modal.open} blur className="py-5">
+        <h3>{modal.message}</h3>
+        <h2 className={modal.color}>👍</h2>
+      </Modal>
       <MarkdownMonaco initialValue={loaderData.content} />
       <Button
         onClick={handleUpdate}
