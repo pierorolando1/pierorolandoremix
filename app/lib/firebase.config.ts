d9bf71f512ca550fffe7de39c8f6c4514df27403
Piero Rolando <rolandopiero63@gmail.com>
2022-03-21 11:48:10 -0400

    nothing

--- app/lib/firebase.config.ts
+++ app/lib/firebase.config.ts
@@ -1,14 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore"
 import { getAuth } from "firebase/auth";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyD4n3nyuos42F48CnjtLsQef9qtc30JR1Q",
   authDomain: "blog-pierorolando.firebaseapp.com",
@@ -25,5 +19,4 @@ export const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
 
 export const auth = getAuth()
-
 //getAnalytics(app);
---
+++ app/lib/helper.server.ts
@@ -0,0 +1,5 @@
+import 'dotenv/config'
+
+export const nose = () => {
+  console.log(process.env.NOSE)
+}
